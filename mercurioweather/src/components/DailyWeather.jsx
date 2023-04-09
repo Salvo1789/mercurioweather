1a0d69f86f0b9d37a9b1dba5b41b52ee03188069
Salvo1789 <salvo.mercurio89@gmail.com>
2023-04-09 21:52:39 +0200

    Dynamic days in dailyweather

--- mercurioweather/src/components/DailyWeather.jsx
+++ mercurioweather/src/components/DailyWeather.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { ListGroup } from "react-bootstrap"
-
+import moment from 'moment';
 
 const DailyWeather = () => {
     const coordinates = useSelector((state) => state.coordinates)
@@ -33,9 +33,10 @@ const DailyWeather = () => {
         <ListGroup>
             {data[0].list.slice(1,6).map((day,i) => (
                 <ListGroup.Item key={i} >
+                  <p>Day: {moment().add(i, 'days').format('dddd')}</p>
+                    <p>Date: {moment().add(i, 'days').format('LL')}</p>
                     <p>Temperature: {day.main.temp}</p>
                     <p>Description: {day.weather[0].main} <img src ={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt='weather description' width="80" height="80"/></p>
-                    
                     <p>Min/Max Temp: {day.main.temp_min.toFixed(0)}° / {day.main.temp_max.toFixed(0)}°</p>
                 </ListGroup.Item>
             )
