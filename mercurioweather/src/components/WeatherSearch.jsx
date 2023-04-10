cbf608fa7a4eeb6fb7b645135e55bfb53f87a031
Salvo1789 <salvo.mercurio89@gmail.com>
2023-04-10 19:16:20 +0200

    error dealing

--- mercurioweather/src/components/WeatherSearch.jsx
+++ mercurioweather/src/components/WeatherSearch.jsx
@@ -4,10 +4,13 @@ import { useDispatch } from 'react-redux'
 import Weather from './Weather'
 import DailyWeather from './DailyWeather'
 import SiteFooter from './SiteFooter'
+import LoadingSpinner from './LoadingSpinner'
 import './styles.css'
 
 const WeatherSearch = () => {
     const [query, setQuery] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
 
     const dispatch = useDispatch()
 
@@ -17,20 +20,23 @@ const WeatherSearch = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-
         const geoUrl = `${process.env.REACT_APP_GEO_API_URL}&q=${query}&appid=${process.env.REACT_APP_API_KEY}`
 
         const fetchWeatherData = async (url) => {
             try{
+                setIsLoading(true)
                 const response = await fetch(url)
                 if (response.ok){
                     const data = await response.json()
+                    setIsLoading(false)
                     return data
-                }else{
+                  }else{
                     console.log('Results fetching error')
-                }
+                  }
+                  
             }catch(error){
-                console.log(error)
+                setErrorMessage('Unable to fetch weather data')
+                setIsLoading(false)
             }
         }
 
@@ -59,10 +65,12 @@ const WeatherSearch = () => {
           </div>
             <div className="row">
             <div className="col-12 col-md-6 mb-2 mb-md-0">
-              <Weather />
+            {isLoading ? <LoadingSpinner /> : <Weather />}
+            {errorMessage && <div className="error">{errorMessage}</div>}
             </div>
             <div className="col-12 col-md-6 mt-2 mt-md-0">
-               <DailyWeather />
+            {isLoading ? <LoadingSpinner /> : <DailyWeather />}
+            {errorMessage && <div className="error">{errorMessage}</div>}   
             </div>
           </div>
           <div className="row">
